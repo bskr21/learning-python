9f65482dfae20cb545636875b0b258a70bb46958
bskr21 <baskara.widhi@gmail.com>
2026-01-12 12:27:40 +0700

    initial commit

---
+++ input-output.py
@@ -0,0 +1,11 @@
+# this program says hello and will determine your age
+
+print('Hello, World')
+print('What is your name') # ask for ther name
+my_name = input('masukin namamu : ')
+print('it is good to meet you, ' + my_name)
+print('The lenght of your name is:')
+print(len(my_name))
+print('what year was your born?') # ask for their year of birth
+my_age = input ('coba isi tahun lahir : ')
+print('Wow, now You are ' + str(2026 - int(my_age)) + ' this year')
