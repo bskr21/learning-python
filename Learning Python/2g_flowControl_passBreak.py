5177fb905b27a34cc2b6c89be769087058e6544a
bskr21 <baskara.widhi@gmail.com>
2026-02-04 12:49:52 +0700

    📖 Learning Pass Break and Continue 📖

--- Learning Python/2g_flowControl_passBreak.py
+++ Learning Python/2g_flowControl_passBreak.py
@@ -7,12 +7,20 @@ for name in names:
     if 'j' in name.lower(): 
         pass # this will skip all names contain 'j' in it
     else:
-        print(name)
+        print(name, '\n')
 
 
 # Break the command
 for name in names:
     if 'h' in name.lower():
         break # the loop stops iterating and breaks after met the condition.
+    else:
+        print(name, '\n')
+
+
+# Continue the command
+for name in names:
+    if 'm' in name.lower():
+        continue
     else:
         print(name)
--- Learning Python/4a_loggingModule.py
+++ Learning Python/4a_loggingModule.py
@@ -7,8 +7,8 @@ logging.debug('start of program')
 def factorial(n):
     logging.debug('Start of Factorial(' + str(n) + ')')
     total = 1
-    for i in range(n + 1):
-        total *= 1
+    for i in range(1, n + 1):
+        total *= i
         logging.debug('i is ' + str(i) + ', total is ' + str(total))
     logging.debug('End of factorial(' + str(n) + ')')
     return total
